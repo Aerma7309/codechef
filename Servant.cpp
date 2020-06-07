2dea5646e8315b4bddf96fdb45475d004410befa
Abhimanyu Maurya <aerma7309@gmail.com>
2020-06-07 23:17:35 +0530

    Servant.cpp

---
+++ Servant.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		if(n<10) cout<<"What an obedient servant you are!"<<"\n";
+		else cout<<-1<<"\n";
+	}
+	return 0;
+}
