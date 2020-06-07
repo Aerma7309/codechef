66767a115089ad4a406c0c464458dd9302bd8269
Abhimanyu Maurya <aerma7309@gmail.com>
2020-06-07 23:17:35 +0530

    GCD_and_LCM.cpp

---
+++ GCD_and_LCM.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long t,a,b;
+	cin>>t;
+	while (t--)
+	{
+		cin>>a>>b;
+
+		cout<<__gcd(a,b)<<" "<<((a*b)/__gcd(a,b))<<"\n";
+	}
+	
+	return 0;
+}
