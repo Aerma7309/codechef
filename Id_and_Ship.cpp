20f294ea64036e5ce875e5b15eb9dcc5f61ee65f
Abhimanyu Maurya <aerma7309@gmail.com>
2020-06-07 23:17:35 +0530

    Id_and_Ship.cpp

---
+++ Id_and_Ship.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	char id;
+	cin>>t;
+	while(t--)
+	{
+		cin>>id;
+		switch (id)
+		{
+		case 'B':
+		case 'b': cout <<"BattleShip\n";
+			break;
+		case 'C':
+		case 'c': cout<<"Cruiser\n";
+			break;
+		case 'D':
+		case 'd': cout<<"Destroyer\n";
+			break;
+		case 'F':
+		case 'f': cout<<"Frigate\n";
+		}
+	}
+	return 0;
+}
